a7a3bbb0a151d293a25ed22748a655cd364e6027
João Parreira <n3okill@hotmail.com>
2024-09-24 14:43:25 +0100

    correction

--- src/di/exceptions/InvalidDecoratorError.ts
+++ src/di/exceptions/InvalidDecoratorError.ts
@@ -3,7 +3,6 @@ import { isConstructorType } from "../types/constructor.type";
 export class InvalidDecoratorError extends Error {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   constructor(decorator: string, target: any, message: string = "can only be used in a class") {
-    console.log(target);
     super(
       `Decorator '${decorator}' found on '"${isConstructorType(target) ? target.name : target.toString()}"' ${message}.`,
     );
