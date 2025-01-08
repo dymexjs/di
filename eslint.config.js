5b55ed5cbb91b8d674f6b4d04f58c3b1a273646a
João Parreira <n3okill@hotmail.com>
2025-01-08 14:02:19 +0000

    feat: new config (#3)
    * feat: new config
    
    * fix: publish permissions

--- eslint.config.js
+++ eslint.config.js
@@ -3,10 +3,23 @@ import tseslint from "typescript-eslint";
 import eslintPrettier from "eslint-config-prettier";
 import security from "eslint-plugin-security";
 
+/** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["./src/**/*.ts"] },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+  },
+  {
+    ignores: ["dist"],
+  },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  eslintPrettier,
+  //...tseslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
   security.configs.recommended,
+  eslintPrettier,
+  {
+    rules: {
+      "@typescript-eslint/array-type": ["error", { default: "generic" }],
+    },
+  },
 ];
